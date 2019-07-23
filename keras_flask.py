30fa8f68a1f5d3f5dc5c33849926f6c615d2133d
vikramforsk2019 <vsg16492cse2016@gmail.com>
2019-07-23 22:35:02 +0530

    modify

--- keras_flask.py
+++ keras_flask.py
@@ -32,7 +32,7 @@ def convertImage(imgData1):
 	 with open('output.png', 'wb') as output:
 		   output.write(base64.b64decode(imgstr))
 
-@app.route('/home')
+@app.route('/')
 def index():
  return render_template("index.html")
 
